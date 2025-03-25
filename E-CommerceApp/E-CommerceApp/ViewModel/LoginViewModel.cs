33d0900e12ff6fc6a0df1394a96be0c947a0b3c0
vaibhavdukane10 <vaibhavdukane10@gmail.com>
2025-03-25 09:29:55 +0530

    Adding ViewModel Classes -LoginViewModel And RegisterViewModel

---
+++ E-CommerceApp/E-CommerceApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceApp.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ E-CommerceApp/E-CommerceApp/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceApp.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [DisplayName("Full Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        [DisplayName("Billing Address")]
+        public string BillingAddress { get; set; }    
+
+
+
+
+
+
+    }
+}
